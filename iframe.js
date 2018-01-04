767178f6fbe013a1aa6897814e40e8b4c1514fa6
securesitepolicy <35103775+securesitepolicy@users.noreply.github.com>
2018-01-04 09:46:19 -0500

    Create iframe.js

---
+++ iframe.js
@@ -0,0 +1,2 @@
+document.write('<style type="text/css">body{padding); margin:0}</style>');
+document.write('<iframe id="iwonder_hack" src="//' + location.host + '/" width="100%" height="100%" style="border:0;padding:0;margin:0"></iframe>');
